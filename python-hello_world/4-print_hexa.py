100b9f05e57d1c4a42e9709294b65774e0255d7f
Kiharalouis <kiharalouis@gmail.com>
2023-10-24 18:26:26 +0300

    innit

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# Iterate through numbers from 0 to 98 (inclusive)
 for i in range(99):
-    # Print the current number in decimal and hexadecimal formats
-    # {:d} is replaced by the decimal representation of the number
-    # {:x} is replaced by the lowercase hexadecimal representation of the number
     print("{:d} = 0x{:x}".format(i, i))
